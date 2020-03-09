88f0052faeda85e1aeb4a6683e713e6bf295a78f
TM <tamaramichaud@hotmail.com>
2020-03-09 21:10:50 +0000

    add: prototypes using copy constructors

---
+++ src/main/java/Creational/PrototypeMaybe/Prototype.java
@@ -0,0 +1,73 @@
+package Creational.PrototypeMaybe;
+
+// when it's easier to copy an existing object than to fully initialize a new one
+
+// an existing (partially or fully constructed) design is called a Prototype
+
+// we make a copy (aka clone the prototype) and customise it
+// we facilitate this process by implementing a factory
+
+// DO NOT USE CLONEABLE.  it's clone() method is simply return super.clone(); -> a shallow clone.
+// for a deep clone you'd need to override this method all over town and it gives no indication of wtf it's really doing.
+
+// so. Copy Constructors.
+
+public class Prototype {
+    public static void main(String[] args) {
+        Employee john = new Employee("John", new Address("123 some street", "johnstown", "usa"));
+        Employee jane = new Employee(john);
+        jane.name = "Jane";
+        jane.address.country = "uk";
+
+        System.out.println(john);
+        System.out.println(jane);
+    }
+}
+
+@SuppressWarnings("WeakerAccess")
+class Address {
+    public String streetAddress, city, country;
+
+    public Address(String streetAddress, String city, String country) {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.country = country;
+    }
+
+    public Address(Address other) {
+        this(other.streetAddress, other.city, other.country);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "streetAddress='" + streetAddress + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
+
+@SuppressWarnings({"WeakerAccess", "unused"})
+class Employee {
+    public String name;
+    public Address address;
+
+    public Employee(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public Employee(Employee other) {
+        this.name = other.name;
+        this.address = new Address(other.address);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
