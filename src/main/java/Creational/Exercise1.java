2e1164e33e7f94620a8c85c9a0dccc5d733e27b1
TM <tamaramichaud@hotmail.com>
2020-03-09 17:16:09 +0000

    add: results of ex1 + unit test

---
+++ src/main/java/Creational/Exercise1.java
@@ -0,0 +1,92 @@
+package Creational;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+
+        new CodeBuilderTest().emptyTest();
+        new CodeBuilderTest().personTest();
+
+        CodeBuilder cb = new CodeBuilder("Person").addField("name", "String").addField("age", "int");
+        System.out.println(cb);
+    }
+}
+
+class CodeBuilder {
+    private SomeCode theCode;
+
+    public CodeBuilder(String className) {
+        this.theCode = new SomeCode(className);
+    }
+
+    public CodeBuilder addField(String name, String type) {
+        this.theCode.setField(name, type);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("public class %s\n{\n", this.theCode.getClassName()));
+        for (String field : this.theCode.getFieldsList()) {
+            sb.append(field);
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+}
+
+class SomeCode {
+
+    private String className;
+    private List<String> fieldsList;
+
+    public SomeCode(String className) {
+        this.className = className;
+        this.fieldsList = new ArrayList<>();
+    }
+
+    public void setField(String name, String type) {
+        this.fieldsList.add(String.format("  public %s %s;\n", type, name));
+    }
+
+    public String getClassName() {
+        return this.className;
+    }
+
+    public List<String> getFieldsList() {
+        return this.fieldsList;
+    }
+}
+
+
+class CodeBuilderTest {
+
+    private String preprocess(String text) {
+        return text.replace("\r\n", "\n").trim();
+    }
+
+    @Test
+    public void emptyTest() {
+        CodeBuilder cb = new CodeBuilder("Foo");
+        assertEquals("public class Foo\n{\n}",
+                preprocess(cb.toString()));
+    }
+
+    @Test
+    public void personTest() {
+        CodeBuilder cb = new CodeBuilder("Person")
+                .addField("name", "String")
+                .addField("age", "int");
+        assertEquals("public class Person\n{\n" +
+                        "  public String name;\n" +
+                        "  public int age;\n}",
+                preprocess(cb.toString()));
+    }
+}
