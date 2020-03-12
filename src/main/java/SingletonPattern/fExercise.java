1ceb8187ec1491d3892ea5027d82badd4ddeec7f
TM <tamaramichaud@hotmail.com>
2020-03-12 20:33:44 +0000

    add: summary of singleton

--- src/main/java/SingletonPattern/fExercise.java
+++ src/main/java/SingletonPattern/fExercise.java
@@ -5,7 +5,7 @@ import java.util.function.Supplier;
 // singletons summary
 // making a 'safe' singleton is easy; construct a static Lazy<T> and return it's value
 // make your singleton an implementation of an abstract class for testability
-// consider defining singleton lifetime in dependency injection container (..?)
+// consider defining singleton lifetime in dependency injection container (spring stuff i think)
 
 public class fExercise {}
 
