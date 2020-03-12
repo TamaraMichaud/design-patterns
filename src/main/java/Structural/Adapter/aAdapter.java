43b72e376165f91418efee3c1e4563d416c811fb
TM <tamaramichaud@hotmail.com>
2020-03-12 20:58:43 +0000

    add: notes about Adapter structural design pattern

---
+++ src/main/java/Structural/Adapter/aAdapter.java
@@ -0,0 +1,11 @@
+package Structural.Adapter;
+
+public class aAdapter {
+
+    // this is all about getting the interface you want from the interface you were given..
+    // just like a plug adapter; incoming voltage and/or pin-numbers can vary between countries
+    // so your adapter helps convert the in/out pins (from 3 to 2 etc) and voltage accordingly.
+
+    // a construct which adapts an existing interface X to conform to the required interface Y
+
+}
