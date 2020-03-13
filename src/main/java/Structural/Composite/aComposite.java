ad39acc02b0f00b39e9c98799141f2c7ae69a7e5
TM <tamaramichaud@hotmail.com>
2020-03-13 21:47:24 +0000

    add: summary notes on composite design pattern

--- src/main/java/Structural/Composite/aComposite.java
+++ src/main/java/Structural/Composite/aComposite.java
@@ -7,5 +7,12 @@ package Structural.Composite;
 // composition is what lets us make compound objects.
 // e.g. shapes, resize() affects all shapes the same (?)
 
+
+// summary: composite design pattern lets us treat objects and collections in the same way
+// java supports container iteration with the Iterable<T> interface
+// this way a single object is able to masquerade as a collection by returning a single-element collection
+// containing only this
+
+
 public class aComposite {
 }
