28de4d0b1fade4a33772492a36784cf76563fbba
TM <tamaramichaud@hotmail.com>
2020-03-13 21:05:14 +0000

    add: composite pattern notes

---
+++ src/main/java/Structural/Composite/aComposite.java
@@ -0,0 +1,11 @@
+package Structural.Composite;
+
+// treat individual or scalar components, as well as aggregate objects or collections of components uniformly
+// so both of these can be treated in the same way
+
+// why? we know that objects can use other objects fields or methods; either via inheritance, or composition.
+// composition is what lets us make compound objects.
+// e.g. shapes, resize() affects all shapes the same (?)
+
+public class aComposite {
+}
