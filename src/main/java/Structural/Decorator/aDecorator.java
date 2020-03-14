ab24be99a7d80e672f90ab810315c25d503edac2
TM <tamaramichaud@hotmail.com>
2020-03-14 12:04:11 +0000

    add: notes about decorator pattern

---
+++ src/main/java/Structural/Decorator/aDecorator.java
@@ -0,0 +1,14 @@
+package Structural.Decorator;
+
+// augment an object with additional functionality
+// without changing the object itself (clashes with open/close and possibly single-responsibility if the new function should be separate)
+// need to be able to interact with existing structures
+// two options:
+//      inherit from required object if possible; some classes are final - e.g. String
+//      build a decorator. which simply references the decorated objects
+
+// decorator pattern: facilitates the addition of behaviours into individual objects without inheriting from them
+
+
+public class aDecorator {
+}
