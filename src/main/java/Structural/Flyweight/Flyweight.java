38d13e506b32b7c63c0ce649ef9396eede1e86d9
TM <tamaramichaud@hotmail.com>
2020-03-14 18:15:45 +0000

    add: flyweight pattern notes

---
+++ src/main/java/Structural/Flyweight/Flyweight.java
@@ -0,0 +1,14 @@
+package Structural.Flyweight;
+
+// space optimisation!
+// avoid redundancy when storing data
+// e.g. mmorpg -> lots of people with the same name. no point storing them all separately;
+//      simply store a list of unique names and a pointer to the name in question (or index for that name)
+// e.g. bold/italic text -> you dont care about each individual character that needs to be made bold/italic,
+//      simply the start-end points in the string
+
+// a space optimisation technique where we store externally the data associated with similar objects
+
+
+public class Flyweight {
+}
