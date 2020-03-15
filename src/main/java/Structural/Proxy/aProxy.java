82655c019747d7d89b0ae3c80176c1a551d19e26
TM <tamaramichaud@hotmail.com>
2020-03-15 14:43:14 +0000

    add: initial notes about proxy pattern

---
+++ src/main/java/Structural/Proxy/aProxy.java
@@ -0,0 +1,15 @@
+package Structural.Proxy;
+
+// an interface for accessing a particular resource
+// e.g. foo.bar()
+// if you decided to move all your foo-related operations elsewhere
+// would you need to change your code?
+// not if you use a proxy!
+// comes in many types; connection proxy, logging, virtual, guarding
+
+// definition: a class that functions as an interface to a particular resource.
+//      that resource may be remote, expensive to construct, or may require logging
+//      or some other added functionality
+
+public class aProxy {
+}
