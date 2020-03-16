516ee641b247dd162eb3fb4845e868f34326080c
TM <tamaramichaud@hotmail.com>
2020-03-16 16:19:15 +0000

    add: begin to code example of command pattern with undo (currently buggy intentionally)

---
+++ src/main/java/Behavioural/Command/aCommand.java
@@ -0,0 +1,119 @@
+package Behavioural.Command;
+
+// YOU SHALL NOT PASS!
+// when you make a statement in java, it is perishable -> you can't just "undo" e.g. a field-assignment
+// you cannot serialize actions into memory so that you can roll them back...
+// what you'd need is an object to represent an operation. e.g. X should do w(). or X should change field Y to = Z.
+//
+// command pattern is an object which represents an instruction to perform a particular action
+// contains all the information required so that the action can be taken
+
+// e.g. bank account modification
+
+import javax.swing.*;
+import java.util.Collections;
+import java.util.List;
+
+public class aCommand {
+    public static void main(String[] args) {
+        BankAccount ba = new BankAccount();
+        System.out.println(ba); // balance should be zero
+
+        List<BankAccountCommand> commands = List.of(
+                new BankAccountCommand(ba, BankAccountCommand.Action.DEPOSIT, 100),
+                new BankAccountCommand(ba, BankAccountCommand.Action.WITHDRAW, 900) // << should fail.
+        );
+        for(BankAccountCommand c: commands) {
+            c.call();
+            System.out.println(ba);
+        }
+
+        System.out.print(" -- Reversing Bank Account Changes... maybe -- ");
+        // remember, to undo you must reverse the order as well as the action
+        for(int i = commands.size() -1 ; i >= 0; i--){
+            commands.get(i).undo();
+            System.out.println(ba);
+        }
+
+    }
+}
+
+interface Command {
+    void call();
+    void undo();
+}
+
+class BankAccountCommand implements Command {
+
+    private BankAccount account;
+    public enum Action {
+        DEPOSIT, WITHDRAW
+    }
+    private Action action;
+    private int amount;
+
+    public BankAccountCommand(BankAccount account, Action action, int amount) {
+        this.account = account;
+        this.action = action;
+        this.amount = amount;
+    }
+
+    @Override
+    public void call() {
+        switch(action){
+
+            case DEPOSIT:
+                account.deposit(amount);
+                break;
+            case WITHDRAW:
+                account.withdraw(amount);
+                break;
+        }
+    }
+
+    @Override
+    public void undo(){
+
+        // assuming that deposit and withdrawal are symmetric, so to undo a deposit, we make it a withdrawal etc...
+        // (this assumption is incorrect... i guess because the rejected withdrawal should not have a successful 'undo' ? yes)
+        switch(action){
+
+            case WITHDRAW:
+                account.deposit(amount);
+                break;
+            case DEPOSIT:
+                account.withdraw(amount);
+                break;
+        }
+    }
+}
+
+
+// can deposit and withdraw... but we want to audit these deposits; log them somwhere. without changing this class...
+class BankAccount {
+    private int balance;
+    private int overdraftLimit = -500;
+
+    public void deposit(int amount){
+        balance += amount;
+        System.out.println("Deposited " + amount);
+        System.out.println("Balance is now " + balance);
+    }
+
+    public void withdraw(int amount) {
+        if(balance - amount >= overdraftLimit) {
+            balance -= amount;
+            System.out.println("Withdrew " + amount);
+            System.out.println("Balance is now " + balance);
+        } else {
+            System.out.println(String.format("Woops, withdrawing %d would take you overdrawn", amount));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "balance=" + balance +
+                '}';
+    }
+}
