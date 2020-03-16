9096a2689be920bb04c3af255f250d1f7f15b8ea
TM <tamaramichaud@hotmail.com>
2020-03-16 14:34:44 +0000

    add: notes about chain of responsibility

---
+++ src/main/java/Structural/ChainOfResponsibility/aChainOfResponsibility.java
@@ -0,0 +1,20 @@
+package Structural.ChainOfResponsibility;
+
+// a sequence of handlers processing an event one after the other
+// usually chaings go uphill; i.e. in a company, employee messes up; who's fault; he's corrupt? his boss? CEO is dodgy?
+
+// click a graphical element on a form:
+// button handles the event
+// container may require a refresh,
+// window might do something too... etc upwards
+
+// Command Query Separation (not sure the pertinence of this note but good to have vocabulary)
+// whenever we operate on objects, we separate these operations into:
+// - query:   asking for information. e.g. please tell me your attack value
+// - command: asking for an action or change. e.g. please set your attack value to 2
+// CQS = having separate means of sending commands and queries to enable e.g. directly accessing a field
+
+
+public class aChainOfResponsibility {
+}
+
